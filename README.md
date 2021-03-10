9360f37c6333dddd3f4b4b70a3a5851145754882
Jennifer Phan <j.phan@newmotion.com>
2021-03-10 18:45:07 +0100

    remove template codes, add first components, install dev dependencies

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# AngularSampleProject
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.3.
+# VetDay (Angular)
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './component/add/add.component';
+import { ListComponent } from './component/list/list.component';
+import { SearchComponent } from './component/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
